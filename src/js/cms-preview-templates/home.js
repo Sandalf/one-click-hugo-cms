1822ae8572a5b491290e240be9bbd7a40f27a882
Luis Sandoval <fofonso_97@me.com>
2019-10-27 13:59:44 -0600

    Updated a few styles in home

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -16,13 +16,6 @@ export default class PostPreview extends React.Component {
     return <div>
         <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
 
-        <div className="bg-grey-1 pv4">
-          <div className="flex-l mhn1-l ph3 center mw7">
-            <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}</h2>
-            <p className="w-60-l mb0">{entry.getIn(["data", "blurb", "text"])}</p>
-          </div>
-        </div>
-
         <div className="bg-off-white pv4">
           <div className="ph3 mw7 center">
             <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "intro", "heading"])}</h2>
@@ -31,7 +24,7 @@ export default class PostPreview extends React.Component {
             <div className="flex-ns mhn2-ns mb3">
               {(entry.getIn(["data", "products"]) || []).map((product, i) => <div className="ph2-ns w-50-ns" key={i}>
                 <img src={getAsset(product.get("image"))} alt="" className="center db mb3" style={{width: "240px"}}/>
-                <p>{product.get("text")}</p>
+                <p class="tc">{product.get("text")}</p>
               </div>)}
             </div>
           </div>
